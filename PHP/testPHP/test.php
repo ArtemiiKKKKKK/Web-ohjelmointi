9ff5efcc197eb8f3bbe6efdac1183a6f57dd4b62
Artemii Kemppi <artemii.kempi@vuoksi.fi>
2026-09-22 10:09:52 +0300

    php task 1 suoritettu

--- PHP/testPHP/test.php
+++ PHP/testPHP/test.php
@@ -1,18 +1,30 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>First PHP page</title>
-  </head>
-  <body>
-    <p>There are <?php echo 24 * 60 * 60; ?> seconds in a day.</p>
-    <p>Today is <?php echo date("jnY"); ?>.</p>
-    <p>The server is running PHP version <?php echo PHP_VERSION; ?>.</p>
-    <?php
-    echo "<ul>";
-    for ($i = 1; $i <= 10; $i++) {
-        echo "<li>" . $i;
-    }
-    echo "</ul>";
-    ?>
-  </body>
+
+<head>
+  <title>First PHP page</title>
+</head>
+
+<body>
+  <p>There are <?php echo 24 * 60 * 60; ?> seconds in a day.</p>
+  <p>Today is <?php echo date("jnY"); ?>.</p>
+  <p>The server is running PHP version <?php echo PHP_VERSION; ?>.</p>
+  <?php
+  echo "<ul>";
+  for ($i = 1; $i <= 10; $i++) {
+    echo "<li>" . $i;
+  }
+  echo "</ul>";
+
+  $greeting = "Moro";
+  $times = 5;
+  $start = 0;
+
+  while ($start <= $times) {
+    echo " $greeting<br> ";
+    $start++;
+  }
+  ?>
+</body>
+
 </html>
